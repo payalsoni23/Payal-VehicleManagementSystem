95cc2b2892c2a8aea2e7eb86c5e026f3bb75a30a
payalsoni23 <payalwork23@gmail.com>
2024-03-14 00:00:31 +0000

    added open api docs

--- src/main/java/com/AA/VehicleManagementSystem/VehicleManagementSystemApplication.java
+++ src/main/java/com/AA/VehicleManagementSystem/VehicleManagementSystemApplication.java
@@ -1,13 +1,16 @@
 package com.AA.VehicleManagementSystem;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Vehicle Management System", version = "1.0", description = "REST API for managing vehicles data"))
 public class VehicleManagementSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VehicleManagementSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleManagementSystemApplication.class, args);
+    }
 
 }
--- src/main/java/com/AA/VehicleManagementSystem/controller/VehicleController.java
+++ src/main/java/com/AA/VehicleManagementSystem/controller/VehicleController.java
@@ -2,6 +2,9 @@ package com.AA.VehicleManagementSystem.controller;
 
 import com.AA.VehicleManagementSystem.domain.Vehicle;
 import com.AA.VehicleManagementSystem.service.VehicleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,26 +15,28 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/vehicleManagementSystem")
+@Tag(name = "Vehicles")
 public class VehicleController {
+
 
     @Autowired
     VehicleService vehicleManagementService;
 
-    //   @Operation(summary = "Add new vehicle details.")
+    @Operation(description = "REST endpoint to add new vehicle data", responses = {@ApiResponse(description = "Success", responseCode = "201"), @ApiResponse(description = "Bad Request", responseCode = "400")})
     @PostMapping("/addVehicle")
     public ResponseEntity<Vehicle> addVehicle(@Valid @RequestBody Vehicle vehicle) {
         var newVehicle = vehicleManagementService.addVehicle(vehicle);
         return ResponseEntity.status(HttpStatus.CREATED).body(newVehicle);
     }
 
-    //   @Operation(summary = "Update vehicle with given VRN.")
+    @Operation(description = "REST endpoint to update vehicle data", responses = {@ApiResponse(description = "Success", responseCode = "200"), @ApiResponse(description = "Bad Request", responseCode = "400")})
     @PutMapping("/updateVehicle")
     public ResponseEntity<Vehicle> updateVehicle(@RequestParam String vrn, @RequestBody Vehicle vehicle) {
         var updatedVehicle = vehicleManagementService.updateVehicle(vrn, vehicle);
         return ResponseEntity.status(HttpStatus.OK).body(updatedVehicle);
     }
 
-    //    @Operation(summary = "Find all the vehicles with given VRN.")
+    @Operation(description = "REST endpoint to list vehicles data for given VRNs", responses = {@ApiResponse(description = "Success", responseCode = "200")})
     @GetMapping("/find")
     public ResponseEntity<List<Vehicle>> getVehicles(@RequestParam List<String> vrnList) {
         var vehicles = vehicleManagementService.getVehicles(vrnList);
